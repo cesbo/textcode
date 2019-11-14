e52b5fb3585bf69aeb5adc85184f59e2365adc07
Andrey Dyldin <and@cesbo.com>
2019-11-15 00:57:22 +0500

    iso6937 fix/tests

--- src/charset/mod.rs
+++ src/charset/mod.rs
@@ -1,5 +1,4 @@
-mod iso6937;
-pub use iso6937::*;
+pub mod iso6937;
 
 mod iso8859;
 pub use iso8859::*;
---
+++ tests/iso6937.rs
@@ -0,0 +1,20 @@
+use textcode::*;
+
+
+#[test]
+fn test_iso6937() {
+    let u = "iso6937 with € sign";
+    let c: &[u8] = &[
+        0x69, 0x73, 0x6f, 0x36, 0x39, 0x33, 0x37, 0x20,
+        0x77, 0x69, 0x74, 0x68, 0x20, 0xa4, 0x20, 0x73,
+        0x69, 0x67, 0x6e,
+    ];
+
+    let mut dst: Vec<u8> = Vec::new();
+    iso6937::encode(u, &mut dst);
+    assert_eq!(dst.as_slice(), c);
+
+    let mut dst = String::new();
+    iso6937::decode(c, &mut dst);
+    assert_eq!(u, dst.as_str());
+}
