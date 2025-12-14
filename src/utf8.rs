775253e4557de40f23079c9be594f45338a4dad7
Andrei Dyldin <and@cesbo.com>
2025-12-14 18:02:10 +0200

    new decode(), decode_to_slice(), encode(), encode_to_slice() api for utf8

--- src/utf8.rs
+++ src/utf8.rs
@@ -1,24 +1,51 @@
 //! UTF-8
 
-pub fn encode(src: &str, dst: &mut Vec<u8>) {
-    dst.extend_from_slice(src.as_bytes());
-}
+use std::io::Write;
 
-pub fn decode(src: &[u8], dst: &mut String) {
+use crate::{
+    TextcodeError,
+    write_decode_fallback,
+};
+
+fn decode_impl<W: Write, R: AsRef<[u8]>>(src: R, dst: &mut W) -> Result<usize, TextcodeError> {
+    let src = src.as_ref();
+
     match std::str::from_utf8(src) {
-        Ok(v) => dst.push_str(v),
-        _ => dst.push('�'),
+        Ok(v) => {
+            dst.write_all(v.as_bytes()).map_err(|_| TextcodeError::Io)?;
+            Ok(v.len())
+        }
+        Err(_) => write_decode_fallback(dst),
     }
 }
 
-pub fn encode_to_vec(src: &str) -> Vec<u8> {
-    let mut ret = Vec::new();
-    encode(src, &mut ret);
-    ret
+fn encode_impl<W: Write, R: AsRef<str>>(src: R, dst: &mut W) -> Result<usize, TextcodeError> {
+    let src = src.as_ref();
+    dst.write_all(src.as_bytes())
+        .map_err(|_| TextcodeError::Io)?;
+    Ok(src.len())
 }
 
-pub fn decode_to_string(src: &[u8]) -> String {
-    let mut ret = String::new();
-    decode(src, &mut ret);
-    ret
+pub fn encode(src: &str) -> Result<Vec<u8>, TextcodeError> {
+    let mut ret = Vec::new();
+    encode_impl(src, &mut ret)?;
+    Ok(ret)
+}
+
+pub fn encode_to_slice(src: &str, dst: &mut [u8]) -> usize {
+    let mut cursor = std::io::Cursor::new(dst);
+    encode_impl(src, &mut cursor).unwrap_or(0)
+}
+
+pub fn decode(src: &[u8]) -> Result<String, TextcodeError> {
+    let mut result = String::new();
+    // SAFE: writes valid UTF-8 sequences or DECODE_FALLBACK
+    let dst = unsafe { result.as_mut_vec() };
+    decode_impl(src, dst)?;
+    Ok(result)
+}
+
+pub fn decode_to_slice(src: &[u8], dst: &mut [u8]) -> usize {
+    let mut cursor = std::io::Cursor::new(dst);
+    decode_impl(src, &mut cursor).unwrap_or(0)
 }
--- tests/utf8.rs
+++ tests/utf8.rs
@@ -1,24 +1,15 @@
 use textcode::*;
 
-
 #[test]
 fn test_utf8() {
     let u = "тест";
     let c: &[u8] = &[0xd1, 0x82, 0xd0, 0xb5, 0xd1, 0x81, 0xd1, 0x82];
 
-    let mut dst = Vec::new();
-    utf8::encode(u, &mut dst);
-    assert_eq!(c, dst.as_slice());
+    let enc = utf8::encode(u).unwrap();
+    assert_eq!(c, enc.as_slice());
 
-    let enc = utf8::encode_to_vec(u);
-    assert_eq!(enc, dst);
-
-    let mut dst = String::new();
-    utf8::decode(c, &mut dst);
-    assert_eq!(u, dst.as_str());
-
-    let dec = utf8::decode_to_string(c);
-    assert_eq!(dec, dst);
+    let dec = utf8::decode(c).unwrap();
+    assert_eq!(u, dec.as_str());
 }
 
 #[test]
@@ -26,34 +17,19 @@ fn test_utf8_n_bytes() {
     let u = "n тест😹x";
     let c: &[u8] = &[
         // 1: 'n'
-        0x6e,
-        // 2: ' '
-        0x20,
-        // 3..4: 'т'
-        0xd1, 0x82,
-        // 5..6: 'е'
-        0xd0, 0xb5,
-        // 7..8: 'с'
-        0xd1, 0x81,
-        // 9..10: 'т'
-        0xd1, 0x82,
-        // 11..14: '😹'
-        0xf0, 0x9f, 0x98, 0xb9,
-        // 15: x
-        0x78]
-    ;
+        0x6e, // 2: ' '
+        0x20, // 3..4: 'т'
+        0xd1, 0x82, // 5..6: 'е'
+        0xd0, 0xb5, // 7..8: 'с'
+        0xd1, 0x81, // 9..10: 'т'
+        0xd1, 0x82, // 11..14: '😹'
+        0xf0, 0x9f, 0x98, 0xb9, // 15: x
+        0x78,
+    ];
 
-    let mut dst = Vec::new();
-    utf8::encode(u, &mut dst);
-    assert_eq!(c, dst.as_slice());
+    let enc = utf8::encode(u).unwrap();
+    assert_eq!(c, enc.as_slice());
 
-    let enc = utf8::encode_to_vec(u);
-    assert_eq!(enc, dst);
-
-    let mut dst = String::new();
-    utf8::decode(c, &mut dst);
-    assert_eq!(u, dst.as_str());
-
-    let dec = utf8::decode_to_string(c);
-    assert_eq!(dec, dst);
+    let dec = utf8::decode(c).unwrap();
+    assert_eq!(u, dec.as_str());
 }
